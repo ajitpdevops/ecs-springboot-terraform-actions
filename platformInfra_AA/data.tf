62a84038f05de6237f5ea28d9630705bac8a7be8
Ajit_Patil <ajitp.devops@outlook.com>
2023-06-20 22:32:06 +0530

    Changes to the secret and repo

--- platformInfra_AA/data.tf
+++ platformInfra_AA/data.tf
@@ -1,8 +1,11 @@
 data "aws_secretsmanager_secret" "secrets" {
-  arn = data.terraform_remote_state.baseinfra.outputs.secret_manager_arn
+  name = "psqladmin-${var.environment}"
 }
 
 data "aws_secretsmanager_secret_version" "creds" {
   secret_id = data.aws_secretsmanager_secret.secrets.id
 }
  
+data "aws_ecr_repository" "service" {
+  name = var.service.name
+}
